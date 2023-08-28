a2f30e5f32c7ab9bb2953f29db21a83a89670a1e
Vedant Pandey <vedantpandey434@gmail.com>
2023-08-28 18:52:57 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3); // We're using node js not browser
+
+/*
+console.log(
+    3
+    +
+    3
+)
+// This is bad practice of writing code.
+*/
+
+console.log("Vedant")
+
+let name = "Vedant"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power of 53
+// BigInt
+// string => ""
+// boolean => true/false 
+// null => standalone value 
+// undefined
+// symbol => unique 
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
