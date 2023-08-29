c6d4da1a9f0d8e8c8e391958cf3bdcf788b4e08c
Vedant Pandey <vedantpandey434@gmail.com>
2023-08-29 10:58:34 +0530

    stack and heap understanding

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -17,7 +17,7 @@ const anotherId = Symbol("123")
 const bigNumber = 2343543423n;
 // console.log(bigNumber)
 
-console.log(typeof name)
+// console.log(typeof name)
 
 // Non Primitive(Reference)
 // Arrays, Objects, Functions
@@ -33,4 +33,28 @@ const myFunc = function(){
     console.log("Hello World")
 }
 
-console.log(typeof heroes)
+// console.log(typeof heroes)
+
+
+// ++++++++++++++++++++++ Stack and Heap Management +++++++++++++++++++++++
+
+// Stack(Primitive Datatypes) and Heap(Non-Primitive Datatypes)
+
+let universityName = "Gujarat Technological University"
+let newUniversityName = universityName
+newUniversityName = "Sankalchand Patel University"
+
+// console.log(universityName)
+// console.log(newUniversityName)
+
+let userOne = {
+    email: "vedant@gtu.com",
+    prn: 12345678
+}
+
+let userTwo = userOne
+
+userTwo.email = "vedant@spu.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
