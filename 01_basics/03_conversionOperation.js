92a477f585b913e2a0463f2e1bebcda33b2c1d3e
Vedant Pandey <vedantpandey434@gmail.com>
2023-08-29 09:06:37 +0530

    datatype conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let numericString = "33"
+console.log(typeof(numericString))
+let valueInNumber = Number(numericString)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let nullVariable = null
+console.log(typeof nullVariable)
+valueInNumber = Number(nullVariable)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let undefinedVaribale = undefined
+console.log(typeof undefinedVaribale)
+valueInNumber = Number(undefinedVaribale)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let stringVaribale = "vedant"
+console.log(typeof stringVaribale)
+valueInNumber = Number(stringVaribale)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let boolVariable = true
+console.log(typeof boolVaribale)
+valueInNumber = Number(boolVariable)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
