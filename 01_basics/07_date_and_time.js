214fd43c1577c82cacd1377fdf6584406d036977
Vedant Pandey <vedantpandey434@gmail.com>
2023-08-31 21:59:52 +0530

    date and time

--- 01_basics/07_date_and_time.js
+++ 01_basics/07_date_and_time.js
@@ -1,29 +1,29 @@
 // ++++++++++++++Dates+++++++++++++
 
 let myDate = new Date()
-// console.log(myDate)
-// console.log(myDate.toString())
-// console.log(myDate.toDateString())
-// console.log(myDate.toLocaleString())
-// console.log(myDate.toLocaleDateString())
-// console.log(myDate.toISOString())
-// console.log(myDate.toJSON())
-// console.log(typeof myDate)
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toJSON())
+console.log(typeof myDate)
 
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("01-14-2023")
 
-// console.log(myCreatedDate.toLocaleString())
+console.log(myCreatedDate.toLocaleString())
 
 // +++++++++++++ TimeStamp +++++++++++
 
 let timeStamp = Date.now()
 
-// console.log(timeStamp)
-// console.log(myCreatedDate.getTime())
-// console.log(Math.floor(Date.now()/1000))
+console.log(timeStamp)
+console.log(myCreatedDate.getTime())
+console.log(Math.floor(Date.now()/1000))
 
 let newDate = new Date()
 console.log(newDate)
