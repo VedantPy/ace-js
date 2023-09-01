e48a20edbdcd5e19b9c11e6798358c0e61c69c2b
Vedant Pandey <vedantpandey434@gmail.com>
2023-09-01 20:27:41 +0530

    part two

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,24 +1,24 @@
 const marvel_heroes = ["ironman", "spiderman", "thor"]
 const dc_heroes = ["superman", "batman", "flash"]
 
-// marvel_heroes.push(dc_heroes)
-// console.log(marvel_heroes)
-// console.log(marvel_heroes[3][1])
+marvel_heroes.push(dc_heroes)
+console.log(marvel_heroes)
+console.log(marvel_heroes[3][1])
 
 
-// const all_heroes = marvel_heroes.concat(dc_heroes)
-// console.log(all_heroes)
+const all_heroes = marvel_heroes.concat(dc_heroes)
+console.log(all_heroes)
 
-// const all = [...marvel_heroes, ...dc_heroes]
-// console.log(all)
+const all = [...marvel_heroes, ...dc_heroes]
+console.log(all)
 
-// const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
-// const new_another_array = another_array.flat(Infinity)
-// console.log(new_another_array)
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const new_another_array = another_array.flat(Infinity)
+console.log(new_another_array)
 
-// console.log(Array.isArray("Hitesh"))
-// console.log(Array.from("Hitesh"))
-// console.log(Array.from({name:"Vedant"})) //interesting
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name:"Vedant"})) //interesting
 
 let score1 = 100
 let score2 = 200
