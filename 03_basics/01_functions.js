e6f07730939ecbc41fcd20789c1948a5d8c19827
Vedant Pandey <vedantpandey434@gmail.com>
2023-09-02 16:50:33 +0530

    function with objects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -13,7 +13,7 @@ function sayMyName() {
 //     console.log(number1 + number2)
 // }
 
-function addTwoNumbers(number1, number2){
+function addTwoNumbers(number1, number2) {
     // let result = number1 + number2
     // return result
     return (number1 + number2)
@@ -26,11 +26,43 @@ function addTwoNumbers(number1, number2){
 let result = addTwoNumbers(12, 5)
 // console.log(`Result: ${result}`)
 
-function loginUserMessage(username = "Sam"){
-    if(username === undefined){
+function loginUserMessage(username = "Sam") {
+    if (username === undefined) {
         return "Please enter your name"
     }
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+// ++++++++++++++ Function with Objects +++++++++++++++
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 600, 2000))
+
+const user = {
+    username: "vedant",
+    course: "ace-js"
+}
+
+function handleObject(anyObject) {
+    console.log(`The name of the person is ${anyObject.username} and he is the tutor of the course ${anyObject.course}`)
+}
+handleObject(user)
+
+handleObject({
+    username: "Sam",
+    course: "AI-ML"
+})
+
+const myArray = [100, 200, 300, 400]
+
+function getSecondElement(getArray){
+    return getArray[1]
+}
+
+console.log(getSecondElement(myArray))
+console.log(getSecondElement([20,10,35,70]))
