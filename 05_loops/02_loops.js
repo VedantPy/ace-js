f81a9016f7d6cc340844ebad41d7b53f4f2c8e81
Vedant Pandey <vedantpandey434@gmail.com>
2023-09-04 21:33:46 +0530

    while do while

---
+++ 05_loops/02_loops.js
@@ -0,0 +1,20 @@
+let index = 0
+while(index <= 10){
+    // console.log(`Value of index: ${index}`)
+    index = index + 2;
+}
+
+let myArr = ["shaktiman", "superman", "batman"]
+
+let arr = 0;
+while (arr < myArr.length) {
+    console.log(myArr[arr])
+    arr = arr + 1
+}
+
+// let score = 1
+let score = 11
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score <= 10);
