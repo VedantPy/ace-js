fbf2ce788d23f1b8e96c628d1b5f0a561aceb12a
Vedant Pandey <vedantpandey434@gmail.com>
2023-09-07 23:14:55 +0530

    bmi calculator

---
+++ 07_projects/02_BMI_generator/script.js
@@ -0,0 +1,28 @@
+const form = document.querySelector("form")
+form.addEventListener("submit", function(e){
+    e.preventDefault()
+    const height = parseInt(document.querySelector("#height").value)
+    const weight = parseInt(document.querySelector("#weight").value)
+    const results = document.querySelector("#results")
+    console.log(results)
+
+    if (height === "" || height < 0 || isNaN(height)) {
+        results.innerHTML = `Please give a valid height ${height}`
+    }
+    else if (weight === "" || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `Please give a valid weight ${weight}`
+    }
+    else{
+        const bmi = (weight /(height*height)*10000).toFixed(2)
+        if(bmi < 18.6){
+            results.innerHTML = `<span>${bmi} and he is underweight</span>`
+        }
+        else if(bmi > 18.6 && bmi < 24.9){
+            results.innerHTML = `<span>${bmi} and he is in normal range</span>`
+        }
+        else{
+            results.innerHTML = `<span>${bmi} and he is overweight</span>`
+        }
+    }
+
+})
